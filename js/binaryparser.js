b33c3d8de16c37b97b8d59486fb52648992c543f
Jonas Höglund <firefly@firefly.nu>
2017-10-08 17:38:57 +0200

    ES6-ify js/binaryparser.js

--- js/binaryparser.js
+++ js/binaryparser.js
@@ -1,86 +1,82 @@
-var StandardTypes = { u8:  [ 1,   'Uint8' ],
-                      u16: [ 2,  'Uint16' ],
-                      u32: [ 4,  'Uint32' ],
-                      i8:  [ 1,    'Int8' ],
-                      i16: [ 2,   'Int16' ],
-                      i32: [ 4,   'Int32' ],
-                      f32: [ 4, 'Float32' ],
-                      f64: [ 8, 'Float64' ] }
+const StandardTypes = { u8:  [ 1,   'Uint8' ],
+                        u16: [ 2,  'Uint16' ],
+                        u32: [ 4,  'Uint32' ],
+                        i8:  [ 1,    'Int8' ],
+                        i16: [ 2,   'Int16' ],
+                        i32: [ 4,   'Int32' ],
+                        f32: [ 4, 'Float32' ],
+                        f64: [ 8, 'Float64' ] }
 
-function BinaryParser(buf, types) {
-  this.buffer  = buf
-  this.view    = new DataView(buf)
-  this.types   = types || {}
-  this.cursor  = 0
-  this.flipped = false
-}
-
-BinaryParser.prototype.isEOF = function () {
-  return this.cursor >= this.buffer.byteLength
-}
-
-BinaryParser.prototype.seek = function (offset, whence) {
-  switch (whence) {
-    case 'set': case null: case undefined:
-      this.cursor = offset; break
-    case 'cur': case 'current':
-      this.cursor += offset; break
-    case 'end':
-      this.cursor = this.buffer.byteLength + offset; break
-    default:
-      throw new Error("BinaryParser#seek: bad `whence`: " + whence)
-  }
-  if (this.cursor < 0) this.cursor = 0
-}
-
-BinaryParser.prototype.read = function (type) {
-  var self = this
-
-  if (type.slice(0,4) == 'buf[') {
-    var countS = type.slice(4,-1),
-        count  = countS == '*'? this.buffer.byteLength - this.cursor
-               :                parseInt(countS),
-        res    = this.buffer.slice(this.cursor, this.cursor + count)
-    this.cursor += count
-    return res
-
-  } else if (type in this.types) {
-    var pair = this.types[type](this.cursor, this.buffer, this.view),
-        len  = pair[0],
-        res  = pair[1]
-    this.cursor += len
-    return res
-
-  } else if (type in StandardTypes) {
-    var len  = StandardTypes[type][0],
-        name = StandardTypes[type][1]
-
-    var res  = this.view['get' + name](this.cursor, this.flipped)
-    this.cursor += len
-    return res
-
-  } else if (Array.isArray(type)) {
-    var res = {}
-    type.forEach(function (kv) {
-      kv = extractKV(kv)
-      var key   = kv[0],
-          type  = kv[1]
-      res[key] = self.read(type)
-    })
-    return res
-  }
-
-  function extractKV(obj) {
-    if ('0' in obj && '1' in obj) {
-      return [ obj[0], obj[1] ]
-
-    } else {
-      for (var key in obj) {
-        return [ key, obj[key] ]
-      }
+function extractKV(obj) {
+  if ('0' in obj && '1' in obj) {
+    return [ obj[0], obj[1] ]
+  } else {
+    for (var key in obj) {
+      return [ key, obj[key] ]
     }
-    throw new Error("No key-value pair found!")
   }
+  throw new Error("No key-value pair found!")
 }
 
-export default BinaryParser
+export default class BinaryParser {
+  constructor(buf, types) {
+    this.buffer  = buf
+    this.view    = new DataView(buf)
+    this.types   = types || {}
+    this.cursor  = 0
+    this.flipped = false
+  }
+
+  isEOF() {
+    return this.cursor >= this.buffer.byteLength
+  }
+
+  flipEndianness() {
+    this.flipped ^= true
+  }
+
+  seek(offset, whence = 'set') {
+    switch (whence) {
+      case 'set':                 this.cursor = offset; break
+      case 'cur': case 'current': this.cursor += offset; break
+      case 'end':                 this.cursor = this.buffer.byteLength + offset; break
+      default:
+        throw new Error("BinaryParser#seek: bad `whence`: " + whence)
+    }
+    if (this.cursor < 0) this.cursor = 0
+  }
+
+  read(type) {
+    // buf[123] or buf[*]
+    if (type.slice(0,4) == 'buf[') {
+      const countS = type.slice(4,-1),
+            count  = countS == '*'? this.buffer.byteLength - this.cursor
+                   :                Number(countS),
+            res    = this.buffer.slice(this.cursor, this.cursor + count)
+      this.cursor += count
+      return res
+
+    // custom types
+    } else if (type in this.types) {
+      const [len, res] = this.types[type](this.cursor, this.buffer, this.view),
+      this.cursor += len
+      return res
+
+    // primitives
+    } else if (type in StandardTypes) {
+      const [len, name] = StandardTypes[type]
+      const res = this.view['get' + name](this.cursor, this.flipped)
+      this.cursor += len
+      return res
+
+    // structures
+    } else if (Array.isArray(type)) {
+      const res = {}
+      for (let kv of type) {
+        const [key, type] = extractKV(kv)
+        res[key] = this.read(type)
+      }
+      return res
+    }
+  }
+}
