027554d598d06e8e4388a1e59129fd06451f3308
Jonas Höglund <firefly@firefly.nu>
2017-10-08 17:26:09 +0200

    Setup babel and uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,31 @@
 const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './js/index.js',
-  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] },
+      }
+    ],
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        ie8: false,
+        ecma: 8,
+        warnings: true,
+      },
+    }),
+  ],
+  devtool: 'source-map',
 }
